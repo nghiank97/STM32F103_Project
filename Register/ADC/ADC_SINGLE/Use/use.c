d75c1e0e67d0cd89c225a4af30256034f5c98924
Khac Nghia <nghia.nk97@gmail.com>
2022-07-15 16:25:28 +0700

    singel adc

---
+++ Register/ADC/ADC_SINGLE/Use/use.c
@@ -0,0 +1,66 @@
+
+#include "use.h"
+
+void gpio_init(void){
+	// Enable clock GPIOC 
+	RCC->APB2ENR |= (1<<4);
+	
+	GPIOC->CRH &=~ 0x00F00000;
+	GPIOC->CRH |= 0x00100000;
+}
+
+void adc_single(void){
+	// 1. Enable clock GPIOA : ADC 1 PA6,PA7 ~ analog mode
+	RCC->APB2ENR |= (1<<2);
+	GPIOA->CRL &=~ 0xFF000000;
+	// 2. Enable clock adc 1
+	RCC->APB2ENR |= (1<<9);
+	// 3. Data Alignment RIGHT
+	ADC1->CR2 &= ~(1<<11);
+	// 4. sample time : 13.5
+	ADC1->SMPR2 &=~(B111<<18);
+	ADC1->SMPR2 |= (2<<18);
+	
+	// 5. External trigger conversion mode for regular channels
+	ADC1->CR2 |= (1<<20);
+	// 6. External event select for regular group
+	ADC1->CR2 |= (B111<<17);
+	
+  // 7. Length convert = 1
+	ADC1->SQR1 &=~(B1111<<20);
+	ADC1->SQR1 |= (0<<20);
+	
+	// 8. Continuous conversion : singel mode
+	ADC1->CR2 &=~ (1<<1);
+	// 9. ADC Convert on 
+	ADC1->CR2 |= (1<<0);
+	HAL_Delay(100);
+}
+
+uint16_t read_adc(){
+	uint16_t adc_value;
+	ADC1->SQR3 |= (6<<0);
+	ADC1->DR = 0;
+	ADC1->CR2 |= (1<<22);
+	while(ADC1->SR&(1<<1));
+	adc_value = ADC1->DR;
+	return adc_value;
+}
+
+extern void setup(void){
+	gpio_init();
+	adc_single();
+}
+
+#define LED_ON() 	{GPIOC->ODR &=~ (1<<13);}
+#define LED_OFF() {GPIOC->ODR |= (1<<13);}
+#define LED_TOGGLE() {GPIOC->ODR ^= (1<<13);}
+
+uint16_t adc_value = 0;
+
+extern void loop(void){
+	LED_TOGGLE();
+	HAL_Delay(100);
+	
+	adc_value = read_adc();
+}
---
+++ Register/ADC/ADC_SINGLE_MULTI/MDK-ARM/RTE/_ADC_SINGLE_MULTI/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ADC_SINGLE_MULTI' 
+ * Target:  'ADC_SINGLE_MULTI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
