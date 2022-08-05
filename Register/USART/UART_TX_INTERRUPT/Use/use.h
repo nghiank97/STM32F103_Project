53358432e75f2506450a6236a8bf26f362fe6560
Khac Nghia <nghia.nk97@gmail.com>
2022-08-05 18:48:28 +0700

    uart rx

---
+++ Register/USART/UART_TX_INTERRUPT/Use/use.h
@@ -0,0 +1,18 @@
+#ifndef USE_H
+#define USE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "main.h"
+#include "binary.h"
+#include <inttypes.h>
+
+extern void setup(void);
+extern void loop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
