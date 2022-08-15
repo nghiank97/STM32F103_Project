df87a01c845209fae6256ff51d01527463593770
Khac Nghia <nghia.nk97@gmail.com>
2022-08-16 02:37:25 +0700

    draw 2

--- Screen/ST7735_DRAW_3LINE/PySerial/main.py
+++ Screen/ST7735_DRAW_3LINE/PySerial/main.py
@@ -8,9 +8,27 @@ try:
 except serial.SerialException as error:
     print ("error : ",error)
 
+x = [0,60,20, 50, 50]
+
 while(1):
-    x = [40,60,20]
-    data = ":{:02x}{:02x}{:02x}\r\n".format(*x)
+    x[0]+=1
+    if x[0]> 60:
+        x[0] = 0
+
+    x[1]+=2
+    if x[1]> 60:
+        x[1] = 20
+
+    x[2]+=4
+    if x[2]> 60:
+        x[2] = 20
+
+    x[3]+=1
+    if x[3]> 60:
+        x[3] = 20
+
+
+    data = ":{:02x}{:02x}{:02x}{:04x}{:04x}\r\n".format(*x)
     print(data)
     connect.write(data.encode())
     time.sleep(0.2)
